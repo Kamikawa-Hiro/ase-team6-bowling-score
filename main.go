b32af0c2fc8a5761c5da0beadcb2299617ad1593
syouma-amuoys <m253663@hiroshima-u.ac.jp>
2025-07-09 10:58:06 +0900

    main.go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import{
+	"fmt"
+}
+
+type player struct{
+	name string
+	score [21]int
+	flame int
+	strike int
+	spare int
+}
+
+func main(){
+	playernum := getplayernum()
+	players := make([]player, playernum)
+	for i:=0; i< playernum; i++{
+		players[i].name := getplayername()
+	}
+
+	for i:=0; i<10; i++{
+		if i == 9{
+			for j:=0; j<playernum; j++{
+				players[j].score[2*i], players[j].score[2*i+1] = getscore_10flame()		//ペアプロで
+				players[j].flame = i
+				printscore(players[j].score, players[j].flame)				
+			}
+		}elif i < 9{
+			for j:=0; j<playernum; j++{
+				players[j].score[2*i], players[j].score[2*i+1] = getscore()
+				players[j].flame = i
+				printscore(players[j].score, players[j].flame)
+			}
+		}
+	}
+}
